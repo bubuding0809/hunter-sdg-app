90dbd59c0f93b0221e95f71d4111f3a0a494bdda
bubuding0809 <bubuding0809@gmail.com>
2023-03-03 12:10:34 +0800

    Added react-native-env then extracted keys to .env file

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,15 +6,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { FIREBASE_API_KEY, FIREBASE_APP_ID } from "@env";
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAi3k8lT7a3cKt0E85wOPnrQ2rXu8DH6I8",
+  apiKey: FIREBASE_API_KEY,
   authDomain: "sdg-app-6f9da.firebaseapp.com",
   projectId: "sdg-app-6f9da",
   storageBucket: "sdg-app-6f9da.appspot.com",
   messagingSenderId: "978460902699",
-  appId: "1:978460902699:web:7631411043a9a5a774716e",
+  appId: FIREBASE_APP_ID,
   measurementId: "G-NXWW1QT3WY",
 };
 
