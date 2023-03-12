cb43b08764425736fa543951506b5533be8cd6e1
Adam Tan <suwekforu@gmail.com>
2023-03-12 18:49:45 +0800

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,172 @@
-import { View, Text, Image, Button } from "react-native";
-import React from "react";
-import { Center } from "native-base";
+import React, { useState } from "react";
+import { Center, Pressable, VStack, Container} from "native-base";
 import { Link, Stack, usePathname, useRouter } from "expo-router";
 import { useFirebaseSession } from "../../context/FirebaseAuthContext";
+import {StyleSheet, FlatList,Modal,View,Text,Button, Image} from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+
 
 export default function Home() {
+
+  const DATA = [{
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    fullName: "Aafreen Khan",
+    timeStamp: "12:47 PM",
+    recentText: "Good Day!",
+    avatarUrl: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  }, {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    fullName: "Sujitha Mathur",
+    timeStamp: "11:11 PM",
+    recentText: "Cheer up, there!",
+    avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU"
+  }, {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    fullName: "Anci Barroco",
+    timeStamp: "6:22 PM",
+    recentText: "Good Day!",
+    avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg"
+  }, {
+    id: "68694a0f-3da1-431f-bd56-142371e29d72",
+    fullName: "Aniket Kumar",
+    timeStamp: "8:56 PM",
+    recentText: "All the best",
+    avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU"
+  }, {
+    id: "28694a0f-3da1-471f-bd96-142456e29d72",
+    fullName: "Kiara",
+    timeStamp: "12:47 PM",
+    recentText: "I will call today.",
+    avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU"
+  }, {
+    id: "28694a0f-3da1-471f-bd96-142456e29d72",
+    fullName: "Kiara",
+    timeStamp: "12:47 PM",
+    recentText: "I will call today.",
+    avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU"
+  }, {
+    id: "28694a0f-3da1-471f-bd96-142456e29d72",
+    fullName: "Kiara",
+    timeStamp: "12:47 PM",
+    recentText: "I will call today.",
+    avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU"
+  },{
+    id: "28694a0f-3da1-471f-bd96-142456e29d72",
+    fullName: "Kiara",
+    timeStamp: "12:47 PM",
+    recentText: "I will call today.",
+    avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU"
+  }];
+
   const router = useRouter();
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const openBounty: () => void = () => {
+    setModalVisible(true);
+    console.log("You pressed");
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
 
   return (
-    <Center
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Link href="/modal">Present modal</Link>
-    </Center>
+    // <SafeAreaView edges={['left', 'right']}>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+
+        <View >
+          <FlatList style = {{borderWidth: 0}}
+            contentContainerStyle={{ flexGrow: 1 }}
+            data = {DATA}
+            renderItem = {({item}) => 
+              <VStack space = {0}>
+                  <Container style = {{flexDirection: "row", borderBottomWidth : 0, margin:5}}>
+                  <Image style={styles.avatar} source={{ uri: item.avatarUrl }} />
+                    <Pressable onPress ={openBounty} style = {styles.pressable}>
+                    <Text style={styles.name_text}>{item.fullName}</Text>
+                    <Text style={styles.description_text}>{item.recentText}</Text>
+                    </Pressable>
+                  </Container>
+                
+              </VStack>
+            
+            }
+          />
+        </View>
+
+
+        <Modal
+          visible = {isModalVisible}
+          animationType = "slide"
+          onRequestClose={closeModal}>
+
+            <View style = {styles.container}>
+              <Text style = {{paddingVertical: 80}}>Modal contents here</Text>
+            </View>
+            <View style = {{paddingBottom:15}}>
+              <Button title="Close" onPress={closeModal} />
+            </View> 
+        </Modal>
+      </View>
+
+    /* </SafeAreaView> */
+
+
+    
   );
 }
+
+
+
+const styles = StyleSheet.create({
+
+  
+pressable: {
+  backgroundColor: "red",
+  width: "100%",
+
+},
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#eaeaea',
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  name_text: {
+    fontWeight: "bold",
+    fontSize: 25
+
+  },
+  description_text: {
+    fontWeight: "normal",
+    fontSize: 15
+  },
+  bountyBox: {
+    width: 100, 
+    height: 100
+  },
+  avatar: {
+    width: 60,
+    height:60,
+    borderRadius:0,
+    marginBottom: 0,
+  },
+});
