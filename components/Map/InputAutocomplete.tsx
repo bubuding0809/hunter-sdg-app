00300caa95bf1fd7194c4023e2c6da52e964a701
bubuding0809 <bubuding0809@gmail.com>
2023-03-16 23:46:17 +0800

    Added effect to set address text

--- components/Map/InputAutocomplete.tsx
+++ components/Map/InputAutocomplete.tsx
@@ -1,24 +1,41 @@
 import {
   GooglePlaceDetail,
   GooglePlacesAutocomplete,
+  GooglePlacesAutocompleteRef,
 } from "react-native-google-places-autocomplete";
 import { StyleSheet, Text } from "react-native";
+import { useEffect, useRef } from "react";
+import { Platform, NativeModules } from "react-native";
+
+const deviceLanguage =
+  Platform.OS === "ios"
+    ? NativeModules.SettingsManager.settings.AppleLocale ||
+      NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13
+    : NativeModules.I18nManager.localeIdentifier;
 
 interface InputAutocompleteProps {
   label: string;
   placeholder?: string;
   onPlaceSelected: (details: GooglePlaceDetail | null) => void;
+  decodedAddress?: string | null;
 }
-
 const InputAutocomplete: React.FC<InputAutocompleteProps> = ({
   label,
   placeholder,
   onPlaceSelected,
+  decodedAddress = "",
 }) => {
+  const ref = useRef<GooglePlacesAutocompleteRef>(null);
+
+  useEffect(() => {
+    decodedAddress && ref.current?.setAddressText(decodedAddress);
+  }, [decodedAddress]);
+
   return (
     <>
       <Text>{label}</Text>
       <GooglePlacesAutocomplete
+        ref={ref}
         styles={{ textInput: styles.input }}
         placeholder={placeholder || ""}
         fetchDetails
@@ -27,7 +44,7 @@ const InputAutocomplete: React.FC<InputAutocompleteProps> = ({
         }}
         query={{
           key: process.env.GOOGLE_MAPS_API_KEY!,
-          language: "pt-BR",
+          language: deviceLanguage,
         }}
       />
     </>
