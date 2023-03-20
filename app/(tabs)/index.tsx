151dd47b4e419b4c3f46727a89f9037830868d12
bubuding0809 <bubuding0809@gmail.com>
2023-03-20 16:25:49 +0800

    Refactoring

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -19,6 +19,76 @@ import useBountiesQuery, {
   BountyQueryType,
 } from "../../utils/scripts/hooks/queries/useGetBounties";
 
+const bountyCard: React.FC<BountyQueryType> = bountyItem => {
+  return (
+    <Center
+      w="full"
+      bg="white"
+      borderWidth={1}
+      rounded="md"
+      shadow={3}
+      flexDirection="row"
+      justifyContent={"start"}
+      alignItems="start"
+      p={3}
+    >
+      {/* Image */}
+      <Image
+        source={{
+          uri: bountyItem.images[0],
+        }}
+        style={{ width: 50, height: 50 }}
+        borderRadius={10}
+      />
+      <Box
+        style={{
+          marginLeft: 10,
+        }}
+        flexShrink={1}
+      >
+        <Text fontSize="xl">{bountyItem.name}</Text>
+        <Text fontSize="md" numberOfLines={2}>
+          {bountyItem.appearance}
+        </Text>
+        <Divider my={2} />
+        <Text fontSize="md" numberOfLines={2}>
+          {bountyItem.additionalInfo?.length > 0
+            ? bountyItem.additionalInfo
+            : "No additional info"}
+        </Text>
+        <Divider my={2} />
+        <Text fontSize="md" textAlign="left">
+          Last seen: {bountyItem.lastSeen.toDate().toString()}
+        </Text>
+      </Box>
+      <Divider orientation="vertical" mx={2} />
+      <Flex direction="column" h="full" grow={1}>
+        <Text fontSize="md" textAlign="right">
+          type: {bountyItem.category}
+        </Text>
+        <Text fontSize="md" textAlign="right">
+          breed: {bountyItem.breed ?? "unknown"}
+        </Text>
+        <Text fontSize="md" textAlign="right">
+          age: {bountyItem.age ?? "unknown"}
+        </Text>
+        <Text fontSize="md" textAlign="right">
+          $$$ {bountyItem.reward ?? "unknown"}
+        </Text>
+        <Text fontSize="md" textAlign="right">
+          Sex: {bountyItem.gender}
+        </Text>
+        <Text fontSize="md" textAlign="right">
+          Lat: {bountyItem.location.toJSON().latitude}
+        </Text>
+        <Text fontSize="md" textAlign="right">
+          Long: {bountyItem.location.toJSON().longitude}
+        </Text>
+      </Flex>
+    </Center>
+  );
+};
+
 const FeedPage = () => {
   const router = useRouter();
   const { data: sessionData, isLoading } = useFirebaseSession();
@@ -26,101 +96,8 @@ const FeedPage = () => {
   // Use query hook to get bounty data
   const { data: bountyData, refetch } = useBountiesQuery();
 
+  // Pull to refresh state
   const [refreshing, setRefreshing] = useState(false);
-
-  // const handleScroll = event => {
-  //   const offsetY = event.nativeEvent.contentOffset.y;
-  //   const contentHeight = event.nativeEvent.contentSize.height;
-  //   const scrollViewHeight = event.nativeEvent.layoutMeasurement.height;
-
-  //   // Check if we're close to the bottom of the list
-  //   if (offsetY >= contentHeight - scrollViewHeight * 1.2) {
-  //     // Get the next 10 bounties
-  //     console.log("Getting next 10 bounties");
-  //   }
-
-  //   // Check if we pulled down to refresh
-  //   if (offsetY <= -60) {
-  //     // Refetch the bounty data
-  //     console.log("Refreshing bounty data");
-  //     if (!refreshing) {
-  //       setRefreshing(true);
-  //       refetch()
-  //         .catch(err => console.log("Error refreshing bounty data: ", err))
-  //         .finally(() => setTimeout(() => setRefreshing(false), 200));
-  //     }
-  //   }
-  // };
-
-  const bountyCard = (bountyItem: BountyQueryType) => {
-    return (
-      <Center
-        w="full"
-        bg="white"
-        borderWidth={1}
-        rounded="md"
-        shadow={3}
-        flexDirection="row"
-        justifyContent={"start"}
-        alignItems="start"
-        p={3}
-      >
-        {/* Image */}
-        <Image
-          source={{
-            uri: bountyItem.images[0],
-          }}
-          style={{ width: 50, height: 50 }}
-          borderRadius={10}
-        />
-        <Box
-          style={{
-            marginLeft: 10,
-          }}
-          flexShrink={1}
-        >
-          <Text fontSize="xl">{bountyItem.name}</Text>
-          <Text fontSize="md" numberOfLines={2}>
-            {bountyItem.appearance}
-          </Text>
-          <Divider my={2} />
-          <Text fontSize="md" numberOfLines={2}>
-            {bountyItem.additionalInfo?.length > 0
-              ? bountyItem.additionalInfo
-              : "No additional info"}
-          </Text>
-          <Divider my={2} />
-          <Text fontSize="md" textAlign="left">
-            Last seen: {bountyItem.lastSeen.toDate().toString()}
-          </Text>
-        </Box>
-        <Divider orientation="vertical" mx={2} />
-        <Flex direction="column" h="full" grow={1}>
-          <Text fontSize="md" textAlign="right">
-            type: {bountyItem.category}
-          </Text>
-          <Text fontSize="md" textAlign="right">
-            breed: {bountyItem.breed ?? "unknown"}
-          </Text>
-          <Text fontSize="md" textAlign="right">
-            age: {bountyItem.age ?? "unknown"}
-          </Text>
-          <Text fontSize="md" textAlign="right">
-            $$$ {bountyItem.reward ?? "unknown"}
-          </Text>
-          <Text fontSize="md" textAlign="right">
-            Sex: {bountyItem.gender}
-          </Text>
-          <Text fontSize="md" textAlign="right">
-            Lat: {bountyItem.location.toJSON().latitude}
-          </Text>
-          <Text fontSize="md" textAlign="right">
-            Long: {bountyItem.location.toJSON().longitude}
-          </Text>
-        </Flex>
-      </Center>
-    );
-  };
 
   return (
     <Center
@@ -136,6 +113,8 @@ const FeedPage = () => {
       }}
     >
       <FlatList
+        // hide scroll bar
+        showsVerticalScrollIndicator={false}
         style={{ width: "100%", minHeight: "100%" }}
         data={bountyData}
         renderItem={({ item }) => bountyCard(item)}
@@ -144,12 +123,9 @@ const FeedPage = () => {
           <RefreshControl
             refreshing={refreshing}
             onRefresh={() => {
+              // Refetch bounties data
               setRefreshing(true);
-              refetch()
-                .catch(err =>
-                  console.log("Error refreshing bounty data: ", err)
-                )
-                .finally(() => setRefreshing(false));
+              refetch().then(err => setRefreshing(false));
             }}
           />
         }
